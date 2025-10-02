bc515746725c13f38e88cf68a255bb1996eee68e
Kirych1995 <u136003@edu.miet.ru>
2025-10-02 22:32:00 +0300

    Add YouTube analytics pipeline

---
+++ src/youtube_parser/__init__.py
@@ -0,0 +1,7 @@
+"""Utilities for collecting analytics from the YouTube channel "Виктор Елисеев БИЗНЕС НАПРОЛОМ"."""
+
+from .pipeline import run_pipeline
+
+__all__ = ["run_pipeline"]
+
+__version__ = "0.1.0"
